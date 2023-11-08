fd9d70bad9807b505655bff1cd02343e02445345
academyKnowledge <academyk74@gmail.com>
2023-11-08 21:00:23 +0200

    lesson 2 - selectors

---
+++ cypress/e2e/lesson2.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+it("lesson1",()=>{
+    // visit command used to visit any URL you want 
+    cy.visit("/");
+
+    cy.get(".links .widget ul li:first-child a");
+
+    cy.get(".logo");
+
+    cy.get(".navigation > ul > li:last-child a").click();
+
+    cy.get(".navigation > ul > li:last-child > a[role=menuitem]").click();
+  
+    cy.get(".copyright");
+
+    cy.contains("Sign In").click();
+
+    cy.get("[name='login[username]']");
+
+    cy.get("#email");
+    
+    cy.get("fieldset.login .email .control input");
+
+});
