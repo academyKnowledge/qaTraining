ae85b9f01a18c7f23acfa5882ba8b5cd66a77f9d
academyKnowledge <academyk74@gmail.com>
2024-01-06 20:45:50 +0200

    API Testing

--- cypress/e2e/It-Describe/api.cy.js
+++ cypress/e2e/It-Describe/api.cy.js
@@ -8,32 +8,40 @@ describe('API Testing', () => {
         // cy.request({options}); // options : url , method , body , log , headers , failOnStatusCode ... 
 
         // http request GET 
-        // GET Used to retrive data from server 
+        // GET method Used to retrieve data from server 
         cy.request("GET","https://jsonplaceholder.typicode.com/")
 
+        // retrieve all posts 
         cy.request("GET","https://jsonplaceholder.typicode.com/posts")
 
+        // retrieve one post .. by sending the id for the post we need 
         cy.request("GET","https://jsonplaceholder.typicode.com/posts/64").then((response)=>{
-           // cy.log(response.body) // object {4}
+           // cy.log(response.body) // object {4} -> id , body ,title , userId 
+
           // API Testing using Cypress  
+          // check the status code for the response we have  
             expect(response.status).to.eq(200)
+
+        // check the details for the response body . 
             expect(response.body.userId).to.eq(7)
             expect(response.body.title).to.contain("et fugit quas eum in in aperiam quod")
         })
 
         // http request POST 
-        // post to send data and create a new record ( post , user , article .... )
+        // post used to send data and create a new record ( post , user , article .... )
         cy.request("POST","https://jsonplaceholder.typicode.com/posts",{"userId":20,"id":500,"title":"Testing","body":"firstTest"})
 
-       // http request PUT 
+        // http request PUT , you should send the post number (98) so the update will be only on it 
+        // if you not send the post number it will fail 404 . 
         cy.request("PUT","https://jsonplaceholder.typicode.com/posts/98",{"title":"update title"})
 
-        // http request DELETE 
+        // http request method DELETE used to delete any record we want 
+        // you should send the record number (98) otherwise it will fail 404 . 
         cy.request("DELETE","https://jsonplaceholder.typicode.com/posts/98")
     });
 
-    it.only('Http requests and responses', () => {
-        // https://reqres.in/api/ 
+    it('Http requests and responses', () => {
+        // https://reqres.in/api/  base Url 
 
         cy.request("GET","https://reqres.in/api/users?page=2").then((res)=>{
             expect(res.status).to.eq(200);
@@ -52,5 +60,6 @@ describe('API Testing', () => {
             expect(response.status).to.eq(201);
             expect(response.body.name).to.eq("morpheus")
         })
+
     });
 });
